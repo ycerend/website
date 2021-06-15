5d70497dff4a40d210d807a2ec55f8aa69e91b51
Ballhausen <ballhausen@bytesforlife.com>
2021-06-15 22:57:51 +0200

    add contact page

---
+++ src/pages/legal/contact.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import { Page, Card, Section, Paragraph, Line } from "../../components/Elements"
+
+export default function Contact() {
+    return (
+        <Page id="legal_contact_heading">
+            <Card id="legal_contact_a">
+                <Paragraph>
+                    <Line id="legal_contact_a1" /><br />
+                    <Line id="legal_contact_a2" /><br />
+                    <Line id="legal_contact_a3" /><br />
+                    <Line id="legal_contact_a4" />
+                </Paragraph>
+                <Paragraph>
+                    <Line id="legal_contact_a5" /><br />
+                    <Line id="legal_contact_a6" /><br />
+                    <Line id="legal_contact_a7" />
+                </Paragraph>
+                <Section id="legal_contact_a8" />
+                <Paragraph id="legal_contact_a9" />
+                <Section id="legal_contact_a10" />
+                <Paragraph id="legal_contact_a11" />
+                <Section id="legal_contact_a12" />
+                <Paragraph id="legal_contact_a13" />
+            </Card>
+        </Page>
+    )
+}
