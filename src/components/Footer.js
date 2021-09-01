fc00a64d370772cb9a74dc402d7d9ce95462a149
Hendrik Ballhausen <44930015+Ballhausen@users.noreply.github.com>
2021-09-01 07:56:19 +0200

    fix and simplify badges

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -47,17 +47,8 @@ export default function Footer(props) {
                     </StyledFooterColumn>
                     <StyledFooterColumn>
                         <p>
-                            <img alt="badge indicating license" src="https://img.shields.io/github/license/fdrtd/api" />
-                        </p>
-                        <p>
+                            <img alt="badge indicating license" src="https://img.shields.io/github/license/fdrtd/website" />
                             <img alt="badge indicating last commit" src="https://img.shields.io/github/last-commit/fdrtd/website" /> &nbsp;&nbsp;&nbsp;&nbsp;
-                            <img alt="badge indicating line count" src="https://img.shields.io/tokei/lines/github/fdrtd/website" /> &nbsp;&nbsp;&nbsp;&nbsp;
-                            <img alt="badge indicating contributor count" src="https://img.shields.io/github/contributors/fdrtd/website" />
-                        </p>
-                        <p>
-                            <img alt="badge indicating issue count" src="https://img.shields.io/github/issues/fdrtd/website" /> &nbsp;&nbsp;&nbsp;&nbsp;
-                            <img alt="badge indicating build status" src="https://img.shields.io/netlify/7f01c714-01c0-4aee-b5ef-443e768a3952" /> &nbsp;&nbsp;&nbsp;&nbsp;
-                            <img alt="badge indicating online status" src="https://img.shields.io/website?url=https%3A%2F%2Fwww.fdrtd.com" />
                         </p>
                     </StyledFooterColumn>
                 </StyledFooterGrid>
