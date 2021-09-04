de6ea52a941a8a537176b2b56b4513ec9f8b0d26
Ballhausen <ballhausen@bytesforlife.com>
2021-09-04 22:39:00 +0200

    simplify landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,10 @@ export default function Layout() {
     return (
         <Page id="welcome">
             <Card id="elevatorpitch">
-                <Columns columns="2">
-                    <div>
-                        <Paragraph id="elevatorpitch_why" />
-                        <Paragraph id="elevatorpitch_how" />
-                        <Paragraph id="elevatorpitch_what" />
-                        <Paragraph id="elevatorpitch_who" />
-                    </div>
-                    <div style={{padding: "2em"}}>
-                        <Image src="/adam-solomon-WHUDOzd5IYU-unsplash.png" credit="Adam Solomon / Unsplash" height="400"/>
-                    </div>
-                </Columns>
+                <Paragraph id="elevatorpitch_why" />
+                <Paragraph id="elevatorpitch_how" />
+                <Paragraph id="elevatorpitch_what" />
+                <Paragraph id="elevatorpitch_who" />
             </Card>
         </Page>
     )
