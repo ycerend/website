7ce4557a645f4c45eecf829d472fab1f06b0052f
Ballhausen <ballhausen@bytesforlife.com>
2021-09-12 21:55:42 +0200

    add content on use cases

--- src/pages/users/usecases.js
+++ src/pages/users/usecases.js
@@ -1,11 +1,51 @@
 import React from "react"
 
-import { Page, Paragraph } from "/src/components/Elements"
+import { Page, Paragraph, Card } from "/src/components/Elements"
 
 export default function Layout() {
     return (
         <Page id="usecases">
-            <Paragraph id="soon_tm" />
+            <Card id="usescases_clinicalstudy">
+                <Paragraph id="usescases_clinicalstudy_1" />
+                <Paragraph id="usescases_clinicalstudy_2" />
+                <Paragraph id="usescases_clinicalstudy_3" />
+            </Card>
+            <Card id="usescases_adriving">
+                <Paragraph id="usescases_adriving_1" />
+                <Paragraph id="usescases_adriving_2" />
+                <Paragraph id="usescases_adriving_3" />
+            </Card>
+            <Card id="usescases_contacttracing">
+                <Paragraph id="usescases_contacttracing_1" />
+                <Paragraph id="usescases_contacttracing_2" />
+                <Paragraph id="usescases_contacttracing_3" />
+            </Card>
+            <Card id="usescases_spysatellites">
+                <Paragraph id="usescases_spysatellites_1" />
+                <Paragraph id="usescases_spysatellites_2" />
+                <Paragraph id="usescases_spysatellites_3" />
+            </Card>
+            <Card id="usescases_supplychain">
+                <Paragraph id="usescases_supplychain_1" />
+                <Paragraph id="usescases_supplychain_2" />
+                <Paragraph id="usescases_supplychain_3" />
+            </Card>
+            <Card id="usescases_iot">
+                <Paragraph id="usescases_iot_1" />
+                <Paragraph id="usescases_iot_2" />
+                <Paragraph id="usescases_iot_3" />
+            </Card>
+            <Card id="usescases_benchmarking">
+                <Paragraph id="usescases_benchmarking_1" />
+                <Paragraph id="usescases_benchmarking_2" />
+                <Paragraph id="usescases_benchmarking_3" />
+                <Paragraph id="usescases_benchmarking_4" />
+            </Card>
+            <Card id="usescases_yours">
+                <Paragraph id="usescases_yours_1" />
+                <Paragraph id="usescases_yours_2" />
+                <Paragraph id="usescases_yours_3" />
+            </Card>
         </Page>
     )
 }
